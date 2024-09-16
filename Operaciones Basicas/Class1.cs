3a5ff2090d8a4856cd6940b62b390425ab4939ac
Javier Carranza <javcarr.2203@gmail.com>
2024-09-15 21:25:24 -0500

    Add project files.

---
+++ Operaciones Basicas/Class1.cs
@@ -0,0 +1,41 @@
+﻿namespace Operaciones_Basicas
+{
+    public class FuncionesBasicas
+    {
+        private bool _blnAceptaNegativos;
+        private bool _blnAceptaDecimales;
+
+        public FuncionesBasicas(bool aceptaNegativos = true, bool aceptaDecimales = true)
+        {
+            this._blnAceptaDecimales = aceptaDecimales;
+            this._blnAceptaDecimales = aceptaNegativos;
+        }
+
+        public double Sumar(List<double> valores) 
+        {
+            double suma = 0;
+
+            foreach (double valor in valores)
+            {
+                if(valor < 0 && this._blnAceptaNegativos == false) throw new Exception("El valor " + valor + " no es aceptado porque no se aceptan negativos");
+                suma += valor;
+            }
+
+            return suma;
+        }
+
+        public double Restar(List<double> valores)
+        { 
+            double resultado = valores[0];
+
+            bool primerValor = false;
+            foreach (double valor in valores)
+            {
+                if(primerValor == false) primerValor = true;
+                else resultado -= valor;
+            }
+
+            return resultado;
+        }
+    }
+}
